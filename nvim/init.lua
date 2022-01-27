a2f8bd2f1a64e39e6b1a0814b0d98ae1b46884b1
Insan Al-Amin <54609220+insanalamin@users.noreply.github.com>
2022-01-27 14:56:31 +0700

    Create init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+-- Packer
+require('plugins')
+-- Config
+require('settings')
+require('settings.color')
+require('settings.font')
+require('settings.completion')
+require('settings.filetype')
+-- LSP Config
+require('lsp')
+-- Keymap
+require('settings.keymap')
