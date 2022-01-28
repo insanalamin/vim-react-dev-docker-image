331c3de2e04fe5cd112a51121819ff6263e4c3ce
Insan Al-Amin <54609220+insanalamin@users.noreply.github.com>
2022-01-28 08:51:20 +0700

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,3 +10,27 @@ require('settings.filetype')
 require('lsp')
 -- Keymap
 require('settings.keymap')
+
+local Plug = require 'plugins.vimplug'
+
+Plug.begin('~/.config/nvim/plugged')
+
+Plug 'moll/vim-bbye'
+Plug('junegunn/goyo.vim', {ft = 'markdown'})
+Plug('VonHeikemen/rubber-themes.vim', {
+  run = function()
+    vim.opt.termguicolors = true
+    vim.cmd('colorscheme rubber')
+  end
+})
+Plug('b3nj5m1n/kommentary', {
+  config = function()
+    local cfg = require('kommentary.config')
+
+    cfg.configure_language('default', {
+      prefer_single_line_comments = true,
+    })
+  end
+})
+
+Plug.ends()
