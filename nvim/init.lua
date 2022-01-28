a95ea1e8991e044d91e177f158511e6f3c1ba528
Insan Al-Amin <54609220+insanalamin@users.noreply.github.com>
2022-01-28 10:15:26 +0700

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,23 +14,18 @@ require('settings.keymap')
 local Plug = require 'plugins.vimplug'
 
 Plug.begin('~/.config/nvim/plugged')
+  -- LSP
+  Plug 'neovim/nvim-lspconfig'
+  Plug 'onsails/lspkind-nvim'
+  Plug('weilbith/nvim-code-action-menu', {
+    cmd = 'CodeActionMenu'
+  })
 
-Plug 'moll/vim-bbye'
-Plug('junegunn/goyo.vim', {ft = 'markdown'})
-Plug('VonHeikemen/rubber-themes.vim', {
-  run = function()
-    vim.opt.termguicolors = true
-    vim.cmd('colorscheme rubber')
-  end
-})
-Plug('b3nj5m1n/kommentary', {
-  config = function()
-    local cfg = require('kommentary.config')
-
-    cfg.configure_language('default', {
-      prefer_single_line_comments = true,
-    })
-  end
-})
-
+  -- Icons
+  Plug 'kyazdani42/nvim-web-devicons'
+  Plug 'ryanoasis/vim-devicons'
 Plug.ends()
+
+require'nvim-tree'.setup {}
+require'lspconfig'.tsserver.setup {}
+require('lspkind').init()
